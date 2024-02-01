8c34d03748eb89d8aa11dd0ed85332b5529ad4a7
karamakcsc <karamakcsc@hotmail.com>
2024-02-01 16:35:07 +0300

    incometax

--- masar_jo_hrms/masar_jo_hrms/doctype/employee_overtime/employee_overtime_list.js
+++ masar_jo_hrms/masar_jo_hrms/doctype/employee_overtime/employee_overtime_list.js
@@ -1,54 +1,54 @@
-frappe.listview_settings['Employee Overtime'] = {
-    onload: function(list) {
-        list.page.add_inner_button(
-            __('Get Attendance Details'),
-            function() {
-                var today = frappe.datetime.nowdate();
-                var firstDayOfMonth = frappe.datetime.month_start(today);
-                var lastDayOfMonth = frappe.datetime.month_end(today);
-                frappe.prompt(
-                    [
-                        {
-                            fieldname: 'date_from',
-                            fieldtype: 'Date',
-                            label: __('From Date'),
-                            'default': firstDayOfMonth,
-                            reqd: 1,
-                            bold: 1
-                        },
-                        {
-                            fieldname: 'date_to',
-                            fieldtype: 'Date',
-                            label: __('To Date'),
-                            'default': lastDayOfMonth,
-                            reqd: 1,
-                            bold: 1
-                        },
-                    ],
-                    function(values) {
-                        frappe.call({
-                            method: 'masar_hrms.masar_hrms.doctype.employee_overtime.employee_overtime.get_employee_attendance',
-                            args: {
-                                date_from: values.date_from,
-                                date_to: values.date_to,
-                            },
-                            callback: function(ret) {},
-                        });
-                        frappe.show_alert({
-                            message: __('Sync has started in the background.'),
-                            indicator: 'green',
-                        });
-                        frappe.socketio.init();
-                        frappe.realtime.on('get_employee_attendance', function() {
-                            list.refresh();
-                        });
-                    },
-                    __('Sync Date Range'),
-                    __('Get')
-                );
-            },
-            null,
-            'primary'
-        );
-    }
-};
+// frappe.listview_settings['Employee Overtime'] = {
+//     onload: function(list) {
+//         list.page.add_inner_button(
+//             __('Get Attendance Details'),
+//             function() {
+//                 var today = frappe.datetime.nowdate();
+//                 var firstDayOfMonth = frappe.datetime.month_start(today);
+//                 var lastDayOfMonth = frappe.datetime.month_end(today);
+//                 frappe.prompt(
+//                     [
+//                         {
+//                             fieldname: 'date_from',
+//                             fieldtype: 'Date',
+//                             label: __('From Date'),
+//                             'default': firstDayOfMonth,
+//                             reqd: 1,
+//                             bold: 1
+//                         },
+//                         {
+//                             fieldname: 'date_to',
+//                             fieldtype: 'Date',
+//                             label: __('To Date'),
+//                             'default': lastDayOfMonth,
+//                             reqd: 1,
+//                             bold: 1
+//                         },
+//                     ],
+//                     function(values) {
+//                         frappe.call({
+//                             method: 'masar_hrms.masar_hrms.doctype.employee_overtime.employee_overtime.get_employee_attendance',
+//                             args: {
+//                                 date_from: values.date_from,
+//                                 date_to: values.date_to,
+//                             },
+//                             callback: function(ret) {},
+//                         });
+//                         frappe.show_alert({
+//                             message: __('Sync has started in the background.'),
+//                             indicator: 'green',
+//                         });
+//                         frappe.socketio.init();
+//                         frappe.realtime.on('get_employee_attendance', function() {
+//                             list.refresh();
+//                         });
+//                     },
+//                     __('Sync Date Range'),
+//                     __('Get')
+//                 );
+//             },
+//             null,
+//             'primary'
+//         );
+//     }
+// };
