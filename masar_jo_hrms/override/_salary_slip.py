9ee091ef91b98e7de81b0f19f990e57ac11ea37c
karamakcsc <karamakcsc@hotmail.com>
2024-03-13 14:38:32 +0300

    income-tax

--- masar_jo_hrms/override/_salary_slip.py
+++ masar_jo_hrms/override/_salary_slip.py
@@ -19,24 +19,16 @@ from frappe.utils import (
 	money_in_words,
 	rounded,
 )
+
 from frappe.utils.background_jobs import enqueue
 
 import erpnext
 from erpnext.accounts.utils import get_fiscal_year
-# from erpnext.loan_management.doctype.loan_repayment.loan_repayment import (
-# 	calculate_amounts,
-# 	create_repayment_entry,
-# )
-# from erpnext.loan_management.doctype.process_loan_interest_accrual.process_loan_interest_accrual import (
-# 	process_loan_interest_accrual_for_term_loans,
-# )
 from erpnext.utilities.transaction_base import TransactionBase
-
+import hrms
 from hrms.hr.utils import get_holiday_dates_for_employee, validate_active_employee
 from hrms.payroll.doctype.additional_salary.additional_salary import get_additional_salaries
-from hrms.payroll.doctype.employee_benefit_application.employee_benefit_application import (
-	get_benefit_component_amount,
-)
+
 from hrms.payroll.doctype.employee_benefit_claim.employee_benefit_claim import (
 	get_benefit_claim_amount,
 	get_last_payroll_period_benefits,
@@ -47,14 +39,105 @@ from hrms.payroll.doctype.payroll_period.payroll_period import (
 	get_period_factor,
 )
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
-# from hrms.payroll.doctype.salary_slip.salary_slip import calculate_tax_by_tax_slab
+from hrms.payroll.doctype.salary_slip.salary_slip import calculate_tax_by_tax_slab
+
+
+
+## Calculate 30 Days ###### Start
+
+def get_working_days_details(self, lwp=None, for_preview=0):
+    payroll_settings = frappe.get_cached_value(
+        "Payroll Settings",
+        None,
+        (
+            "payroll_based_on",
+            "include_holidays_in_total_working_days",
+            "consider_marked_attendance_on_holidays",
+            "daily_wages_fraction_for_half_day",
+            "consider_unmarked_attendance_as",
+        ),
+        as_dict=1,
+    )
+
+    consider_marked_attendance_on_holidays = (
+        payroll_settings.include_holidays_in_total_working_days
+        and payroll_settings.consider_marked_attendance_on_holidays
+    )
+
+    daily_wages_fraction_for_half_day = (
+        flt(payroll_settings.daily_wages_fraction_for_half_day) or 0.5
+    )
+
+    working_days = 30
+    if for_preview:
+        self.total_working_days = working_days
+        self.payment_days = working_days
+        return
+
+    holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
+    working_days_list = [
+        add_days(getdate(self.start_date), days=day) for day in range(0, working_days)
+    ]
+
+    if not cint(payroll_settings.include_holidays_in_total_working_days):
+        working_days_list = [i for i in working_days_list if i not in holidays]
+
+        working_days -= len(holidays)
+        if working_days < 0:
+            frappe.throw(_("There are more holidays than working days this month."))
+
+    if not payroll_settings.payroll_based_on:
+        frappe.throw(_("Please set Payroll based on in Payroll settings"))
+
+    if payroll_settings.payroll_based_on == "Attendance":
+        actual_lwp, absent = self.calculate_lwp_ppl_and_absent_days_based_on_attendance(
+            holidays, daily_wages_fraction_for_half_day, consider_marked_attendance_on_holidays
+        )
+        self.absent_days = absent
+    else:
+        actual_lwp = self.calculate_lwp_or_ppl_based_on_leave_application(
+            holidays, working_days_list, daily_wages_fraction_for_half_day
+        )
+
+    if not lwp:
+        lwp = actual_lwp
+    elif lwp != actual_lwp:
+        frappe.msgprint(
+            _("Leave Without Pay does not match with approved {} records").format(
+                payroll_settings.payroll_based_on
+            )
+        )
+
+    self.leave_without_pay = lwp
+    self.total_working_days = working_days
+
+    payment_days = 30
+
+    if flt(payment_days) > flt(lwp):
+        self.payment_days = flt(payment_days) - flt(lwp)
+
+        if payroll_settings.payroll_based_on == "Attendance":
+            self.payment_days -= flt(absent)
+
+        consider_unmarked_attendance_as = payroll_settings.consider_unmarked_attendance_as or "Present"
+
+        if (
+            payroll_settings.payroll_based_on == "Attendance"
+            and consider_unmarked_attendance_as == "Absent"
+        ):
+            unmarked_days = self.get_unmarked_days(payroll_settings.include_holidays_in_total_working_days)
+            self.absent_days += unmarked_days  # will be treated as absent
+            self.payment_days -= unmarked_days
+    else:
+        self.payment_days = 0
+        
+## Calculate 30 Days ###### END
 
 def calculate_variable_tax(self, tax_component):
 			self.previous_total_paid_taxes = self.get_tax_paid_in_period(
 				self.payroll_period.start_date, self.start_date, tax_component
 			)
 
-			# Structured tax amount
 			eval_locals, default_data = self.get_data_for_eval()
 			self.total_structured_tax_amount = calculate_tax_by_tax_slab(
 				self.total_taxable_earnings_without_full_tax_addl_components,
@@ -62,15 +145,10 @@ def calculate_variable_tax(self, tax_component):
 				self.whitelisted_globals,
 				eval_locals,
 			)
-
-			#self.current_structured_tax_amount =  self.total_structured_tax_amount / 12 #Yasser
-			self.current_structured_tax_amount =  (
-			self.total_structured_tax_amount
-			#- self.previous_total_paid_taxes
-			) / 12
-			#/ self.remaining_sub_periods
-
-			# Total taxable earnings with additional earnings with full tax
+			
+			self.current_structured_tax_amount = (
+			self.total_structured_tax_amount - self.previous_total_paid_taxes
+			) / self.remaining_sub_periods
 			self.full_tax_on_additional_earnings = 0.0
 			if self.current_additional_earnings_with_full_tax:
 				self.total_tax_amount = SalarySlip.calculate_tax_by_tax_slab(
@@ -79,10 +157,9 @@ def calculate_variable_tax(self, tax_component):
 				self.full_tax_on_additional_earnings = self.total_tax_amount - self.total_structured_tax_amount
 
 			current_tax_amount = self.current_structured_tax_amount + self.full_tax_on_additional_earnings
-   			# if flt(current_tax_amount) < 0:
-			# 	current_tax_amount = 0
-			if flt(current_tax_amount) != 0:
-				current_tax_amount = current_tax_amount - 0.004
+			if flt(current_tax_amount) < 0:
+				current_tax_amount = 0
+
 
 			self._component_based_variable_tax[tax_component].update(
 				{
@@ -125,109 +202,16 @@ def compute_taxable_earnings_for_year(self):
 		self.other_incomes = self.get_income_form_other_sources() or 0.0
 
 		# Total taxable earnings including additional and other incomes
-		# if self.deduct_tax_for_unclaimed_employee_benefits == 0:
-			# self.total_taxable_earnings = (self.current_structured_taxable_earnings * 12) - self.total_exemption_amount # Yasser
-		# else:self.total_taxable_earnings = (self.gross_pay
-		self.total_taxable_earnings = (self.gross_pay
-			# self.previous_taxable_earnings
-			# + self.current_structured_taxable_earnings
-			# + self.future_structured_taxable_earnings
-			# + self.current_additional_earnings
+		self.total_taxable_earnings = (
+			self.previous_taxable_earnings
+			+ self.current_structured_taxable_earnings
+			+ self.future_structured_taxable_earnings
+			+ self.current_additional_earnings
 			# + self.other_incomes
 			# + self.unclaimed_taxable_benefits
-			* 12
 		) - self.total_exemption_amount
-		# frappe.msgprint(str(self.total_taxable_earnings))
-		# frappe.msgprint(str(self.current_additional_earnings_with_full_tax))
-		# frappe.msgprint(str(self.current_additional_earnings))
+	
 		# Total taxable earnings without additional earnings with full tax
-		self.total_taxable_earnings_without_full_tax_addl_components =(
-			self.total_taxable_earnings - self.current_additional_earnings_with_full_tax
-		# self.total_taxable_earnings_without_full_tax_addl_components =(
-		#     self.total_taxable_earnings
-		)
+		self.total_taxable_earnings_without_full_tax_addl_components =(self.total_taxable_earnings )
+            
 
-
-# def set_loan_repayment(self):
-#     principal_amounts = []
-#     interest_amounts = []
-
-#     if not self.get("loans"):
-#         for loan in self.get_loan_details():
-#             amounts = calculate_amounts(loan.name, self.posting_date, "Regular Payment")
-#             if amounts["interest_amount"] or amounts["payable_principal_amount"]:
-#                 self.append(
-#                     "loans",
-#                     {
-#                         "loan": loan.name,
-#                         "interest_amount": amounts["interest_amount"],
-#                         "principal_amount": amounts["payable_principal_amount"],
-#                         "loan_account": loan.loan_account,
-#                         "interest_income_account": loan.interest_income_account,
-#                     },
-#                 )
-
-#     for payment in self.get("loans"):
-#         amounts = calculate_amounts(payment.loan, self.posting_date, "Regular Payment")
-#         principal_amount = amounts["payable_principal_amount"]
-#         interest_amount = amounts["interest_amount"]
-#         total_amount = principal_amount + interest_amount
-
-#         if payment.total_payment > total_amount:
-#             frappe.throw(
-#                 _(
-#                     """Row {0}: Paid amount {1} is greater than pending accrued amount {2} against loan {3}"""
-#                 ).format(
-#                     payment.idx,
-#                     frappe.bold(payment.total_payment),
-#                     frappe.bold(total_amount),
-#                     frappe.bold(payment.loan),
-#                 )
-#             )
-
-#         principal_amounts.append(principal_amount)
-#         interest_amounts.append(interest_amount)
-
-#     return principal_amounts, interest_amounts
-		
-def calculate_tax_by_tax_slab(
-	annual_taxable_earning, tax_slab, eval_globals=None, eval_locals=None
-):
-	eval_locals.update({"annual_taxable_earning": annual_taxable_earning})
-	tax_amount = 0
-	for slab in tax_slab.slabs:
-		cond = cstr(slab.condition).strip()
-		if cond and not eval_tax_slab_condition(cond, eval_globals, eval_locals):
-			continue
-		if not slab.to_amount and annual_taxable_earning >= slab.from_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
-			continue
-
-		if annual_taxable_earning >= slab.from_amount and annual_taxable_earning < slab.to_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
-		elif annual_taxable_earning >= slab.from_amount and annual_taxable_earning >= slab.to_amount:
-			tax_amount += (slab.to_amount - slab.from_amount + 1) * slab.percent_deduction * 0.01
-
-	# other taxes and charges on income tax
-	for d in tax_slab.other_taxes_and_charges:
-		if flt(d.min_taxable_income) and flt(d.min_taxable_income) > annual_taxable_earning:
-			continue
-
-		if flt(d.max_taxable_income) and flt(d.max_taxable_income) < annual_taxable_earning:
-			continue
-
-		tax_amount += tax_amount * flt(d.percent) / 100
-
-	return tax_amount
-
-
-def eval_tax_slab_condition(condition, eval_globals=None, eval_locals=None):
-	if not eval_globals:
-		eval_globals = {
-			"int": int,
-			"float": float,
-			"long": int,
-			"round": round,
-			"date": date,
-			"getdate": getdate,
-		}
